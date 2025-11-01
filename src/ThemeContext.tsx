e99e26f5a618102052d3ccac679fd2acd82a8fea
rsu <61874914+rsu-Suba@users.noreply.github.com>
2025-11-01 22:43:23 +0900

    Happy Halloween is gone😭

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -1,11 +1,11 @@
-import { createContext, useContext, useState, useMemo, /* useEffect,*/ ReactNode, Dispatch, SetStateAction } from "react";
+import { createContext, useContext, useState, useMemo, useEffect, ReactNode, Dispatch, SetStateAction } from "react";
 import { ConfigProvider, theme } from "antd";
 import enUS from "antd/lib/locale/en_US";
 import jaJP from "antd/lib/locale/ja_JP";
 import { useTranslation } from "react-i18next";
-// import { accentsColors as colors } from "@/scripts/Data/DataPack";
-// import { getCookie } from "@/scripts/Server/Cookie";
-// import DarkClick from "@/scripts/Data/DarkClick";
+import { accentsColors as colors } from "@/scripts/Data/DataPack";
+import { getCookie } from "@/scripts/Server/Cookie";
+import DarkClick from "@/scripts/Data/DarkClick";
 
 type ThemeContextType = {
     primaryColor: string;
@@ -25,30 +25,26 @@ type ThemeProviderProps = {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export const ThemeProvider = ({ children }: ThemeProviderProps) => {
-    // const root = document.querySelector(":root") as HTMLElement;
+    const root = document.querySelector(":root") as HTMLElement;
     const { i18n } = useTranslation();
-   //  const [primaryColor, setPrimaryColor] = useState(colors[0][0]);
-    const [primaryColor, setPrimaryColor] = useState("#9718ff");
+    const [primaryColor, setPrimaryColor] = useState(colors[0][0]);
     const { defaultAlgorithm, darkAlgorithm } = theme;
-   //  const [isDarkMode, setIsDarkMode] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(true);
+    const [isDarkMode, setIsDarkMode] = useState(false);
     const [isPerformanceMode, setIsPerformanceMode] = useState(true);
     const [localeLang, setLocaleLang] = useState(i18n.languages[0] == "ja" ? jaJP : enUS);
-   //  let darkNum: number = 0;
+    let darkNum: number = 0;
 
-    /*
-   useEffect(() => {
-      if (getCookie("dark") == "true") {
-         setIsDarkMode(true);
-         DarkClick(isDarkMode);
-      }
-      if (getCookie("PrimaryColor") != "") {
-         if (isDarkMode) darkNum = 1;
-         setPrimaryColor(colors[darkNum][parseInt(getCookie("PrimaryColor"))]);
-         root.style.setProperty("--main-color", colors[darkNum][parseInt(getCookie("PrimaryColor"))]);
-      }
-   }, []);
-   */
+    useEffect(() => {
+        if (getCookie("dark") == "true") {
+            setIsDarkMode(true);
+            DarkClick(isDarkMode);
+        }
+        if (getCookie("PrimaryColor") != "") {
+            if (isDarkMode) darkNum = 1;
+            setPrimaryColor(colors[darkNum][parseInt(getCookie("PrimaryColor"))]);
+            root.style.setProperty("--main-color", colors[darkNum][parseInt(getCookie("PrimaryColor"))]);
+        }
+    }, []);
 
     const themeValue = useMemo(
         () => ({
